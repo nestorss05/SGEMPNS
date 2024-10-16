52eabcc1789eb3a65d7e3184e4bbcd311c453178
nestorss05 <nestor.sanchez@iesnervion.es>
2024-10-16 19:32:40 +0200

    Ejercicios Tema 6 realizados

---
+++ Tema6/EjerciciosTema6/Ejercicio2/Controllers/ProductosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio2.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
