fb999a0b3d58a251e1b86ed898359550b8662a89
nestorss05 <nestor.sanchez@iesnervion.es>
2024-11-05 14:38:56 +0100

    Ejercicio T8 Final sin empezar con txt

---
+++ Tema8/EjercicioT8Final/EjercicioT8Final/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EjercicioT8Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
