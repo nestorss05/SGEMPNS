e607bcb3f619e2ad52243f979eec1567946a8b5a
nestorss05 <nestor.sanchez@iesnervion.es>
2024-11-15 14:29:06 +0100

    Ejercicio 2 empezado

---
+++ Tema10/EjerciciosTema10/Ejercicio2/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Ejercicio2.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
