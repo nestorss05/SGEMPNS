6651f186ea8d3291cc7ad583708f8b08b3544f9e
nestorss05 <nestor.sanchez@iesnervion.es>
2024-12-17 14:34:17 +0100

    Ejercicio Pokemon terminado

--- Tema13/PruebasT13/pokeapi/ejercicio.js
+++ Tema13/PruebasT13/pokeapi/ejercicio.js
@@ -1,6 +1,12 @@
+/**
+ * Metodo que llama a la API para obtener el listado de pokemons
+ * Pre: ninguna
+ * Post: ninguna
+ */
 function pedirDatos() {
+
     var ajax = new XMLHttpRequest();
-    ajax.open("GET", "https://pokeapi.co/api/v2/pokemon");
+    ajax.open("GET", "https://pokeapi.co/api/v2/pokemon?limit=135&offset=251");
 
     //Definicion estados
     ajax.onreadystatechange = function () {
@@ -9,28 +15,82 @@ function pedirDatos() {
     } else 
         if (ajax.readyState == 4 && ajax.status == 200) {
             var arrayPokemons = JSON.parse(ajax.responseText);
-            alert(ajax.responseText); // A quitar
             listarPokemon(arrayPokemons)
         }
     };
     ajax.send();
 }
 
+/**
+ * Metodo que pasa al HTML el listado de pokemons en una tabla
+ * Pre: el listado de pokemons debe estar en un formato JSON valido
+ * Post: ninguna
+ * @param {*} arrayPokemons 
+ */
 function listarPokemon(arrayPokemons) {
+    var datos = document.getElementById("datos");
+    datos.innerHTML = "";
     var pokes = document.getElementById("pokemon");
-    var html = "<table border=1>";
-    html += "<tr> <td> ID </td>"
-    html += "<td> Nombre </td>"
-    html += "<td> </td> </tr>"
-    for (i = 0; i < arrayPokemons.results.length; i++) {
-        html += "<tr> <td>";
-        html += i + 1;
-        html += "</td> <td>";
-        html += arrayPokemons.results[i].name;
-        html += "</td> <td>";
-        html += "Detalles (TODO)";
-        html += "</td> </tr>";
+    var html = "<ul>";
+    for (let i = 0; i < arrayPokemons.results.length; i++) {
+        html += `<li>
+                <div class="pokemon-item" onclick="detalles('${arrayPokemons.results[i].url}')">
+                    ${arrayPokemons.results[i].name}
+                </div>
+            </li>`;
     }
-    html += "</table>";
+    html += "</ul>";
     pokes.innerHTML = html;
+}
+
+/**
+ * Metodo que se conecta a la API para obtener el pokemon especificado por ID
+ * Pre: el URL del pokemon en cuestion debe ser valido y debe funcionar
+ * Post: ninguna
+ * @param {*} url 
+ */
+function detalles(url) {
+
+    var ajax = new XMLHttpRequest();
+    ajax.open("GET", url);
+
+    //Definicion estados
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState < 4) {
+            //aquí se puede poner una imagen de un reloj o un texto “Cargando”
+        } else {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                var pokemon = JSON.parse(ajax.responseText);
+                mostrarDetalles(pokemon)
+            }
+        };
+    }
+    
+    ajax.send();
+    
+}
+
+/**
+ * Metodo que pasa los detalles de un pokemon al HTML
+ * Pre: el pokemon debe estar en un formato JSON valido
+ * Post: ninguna
+ * @param {*} pokemon 
+ */
+function mostrarDetalles(pokemon) {
+
+    var detalles = document.getElementById("detalles");
+    detalles.innerHTML = "";
+
+    detalles.innerHTML = `
+        <h3>Detalles del Pokemon</h3>
+        <img src="${pokemon.sprites.front_default}" alt="Sin imagen" style="width: 150px; height: 150px;">
+        <p><strong>Nombre:</strong> ${pokemon.name}</p>
+        <p><strong>ID:</strong> ${pokemon.id}</p>
+        <p><strong>Peso:</strong> ${pokemon.weight}</p>
+        <p><strong>Altura:</strong> ${pokemon.height}</p>
+        <p><strong>Habilidades:</strong> 
+            ${pokemon.abilities.map(a => a.ability.name).join(', ')}
+        </p>
+    `;
+
 }
