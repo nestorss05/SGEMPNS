bbbb437ad0080d990d9a2977111052684ee079bf
nestorss05 <nestor.sanchez@iesnervion.es>
2025-01-21 14:30:58 +0100

    Ejercicio tema 14 rev

--- Tema14/Ejercicios/Ejercicio1/src/app/app.component.ts
+++ Tema14/Ejercicios/Ejercicio1/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TablaPersonasComponent } from './components/tabla-personas/tabla-personas.component';
-import { FormularioPersonaComponent } from './components/formulario-persona/formulario-persona.component';
 
 @Component({
   selector: 'app-root',
